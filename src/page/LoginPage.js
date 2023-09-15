ce16679fe9269057ad6335e21ddecea5a7191875
wnsdnjs0315 <137370522+wnsdnjs0315@users.noreply.github.com>
2023-09-15 16:00:25 +0900

    Add files via upload

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
-const LoginPage = () => {
+const LoginPage = ({setAuthenticate}) => {
+  const navigate = useNavigate();
+
+  //submin 버튼을 클릭할 때마다 form이 매번 새로고침되므로 콘솔창에서 텍스트가 사라짐
+  //form을 쓸 때는 form이 새로고침되는 것을 막아주는 e.preventDefault()를 써준다
+  const loginUser = (e) => {
+    e.preventDefault();
+    console.log("Login user function issue")
+    setAuthenticate(true);
+    navigate('/')
+  }
+
   return (
     <div className='form-area'>
-      <Form>
+      {/* 버튼 type이 submit일 경우에는 onClick 이벤트를 주지 않는다 */}
+      <Form omSubmit={(e)=>loginUser(e)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
